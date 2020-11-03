bae2a6e0c3c4b664ca02c2e8ed0798ce38e79724
Diego Q <diiegg@users.noreply.github.com>
2020-11-03 15:14:33 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAUWGCHDXTNJDLVJNX"
-  secret_key = "/zBuaqm5bHeOzqkHe5rV0id0NRCvf8SdaZD+ssQ6"
+  access_key = "your key here"
+  secret_key = "your key here"
 }
